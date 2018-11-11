b4545c3ac17eaa3278a1428c0f4147901d5d4400
Parvez <rocksparvezmulla1.com>
2018-11-11 21:17:02 +0530

    white noise added

--- BankApi/settings/production.py
+++ BankApi/settings/production.py
@@ -140,9 +140,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
 #STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
 
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
