fcc0c9e88d43e296f330383e66c8ff62f1da3602
Parvez <rocksparvezmulla1.com>
2018-11-11 21:13:20 +0530

    white noise added

--- BankApi/settings/production.py
+++ BankApi/settings/production.py
@@ -143,12 +143,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+#STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 #STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
+#MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
